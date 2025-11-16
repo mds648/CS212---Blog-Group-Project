47044b39e527bb932e7ffe366b3a40df42788eb8
AshCat31 <31ccat520@gmail.com>
2025-11-16 04:19:08 -0700

    add highlighting of current nav link; move like js to post pg only

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
-// like count 
-const likeBtn = document.getElementById("likeBtn");
-const likeCount = document.getElementById("likeCount");
-const likeImg = document.getElementById("likeImg");
-
-// set the initial state of the button to false 
-let liked = false;
-// set the initial count to 0
-let count = 0;
-
-likeBtn.addEventListener("click", function(e){
-    // get the opposite of the current state of the btn
-    liked = !liked;
-    
-    // if liked
-    if (liked) {
-        // change the img
-        likeImg.src = "./images/heart-filled.png";
-        // icrement the likes count
-        count++;
-    }
-    // if disliked
-    else {
-        // change the img
-        likeImg.src = "./images/heart-outline.png";
-        // decrement the count
-        count--;
-    }
-    // update the count
-    likeCount.textContent = count;
-})
+// highlight active link in navbar
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('.navbar-nav .nav-link');
+    const currentUrl = window.location.href; // current url
+    links.forEach(link => {
+        console.log(link.href, currentUrl);
+        if (link.href === currentUrl) {
+            link.classList.add('nav-active');
+        }
+    });
+});

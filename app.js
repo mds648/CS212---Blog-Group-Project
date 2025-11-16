d2eaddb1ac1a69af1904f66687e570d28ec6ae82
AshCat31 <31ccat520@gmail.com>
2025-11-16 05:04:05 -0700

    add link copying to post sharing, can change later

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-        const navbarHTML = `
+function insertNavbar() {
+    const navbarHTML = `
         <nav class="navbar navbar-expand-sm justify-content-center border-bottom border-black">
             <ul class="navbar-nav">
                 <li class="nav-item mx-3"><a class="nav-link" href="index.html">home</a></li>
@@ -15,14 +15,18 @@ document.addEventListener('DOMContentLoaded', function() {
             </form>
         </nav>
         `;
-        document.getElementById('navbar-container').innerHTML = navbarHTML;
-        // highlight active page
-        const currentPage = window.location.pathname.split('/').pop();  // Get the current page's filename
-        const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-        navLinks.forEach(link => {
-            // If the href matches the current page, add the 'active' class
-            if (link.getAttribute('href') === currentPage) {
-                link.classList.add('active');
-            }
-        });
+    document.getElementById('navbar-container').innerHTML = navbarHTML;
+    // highlight active page
+    const currentPage = window.location.pathname.split('/').pop();  // Get the current page's filename
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    navLinks.forEach(link => {
+        // If the href matches the current page, add the 'active' class
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
     });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    insertNavbar();
+});

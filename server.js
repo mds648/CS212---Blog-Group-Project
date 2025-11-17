a1dd331e95962ea6c0895b36796735bf438d7ab0
AJ Montanez <ale2395784@maricopa.edu>
2025-11-17 16:23:47 -0700

    administration

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+function getUsers() {
+    return JSON.parse(fs.readFileSync("users.json"));
+}
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    const users = getUsers();
+
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (!user) {
+        return res.status(401).json({ success: false, message: "Invalid login" });
+    }
+
+    res.json({
+        success: true,
+        username: user.username,
+        role: user.role
+    });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+

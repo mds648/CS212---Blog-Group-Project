fdff068806d008a98545dfd54f689e210973d033
AshCat31 <31ccat520@gmail.com>
2025-11-17 20:02:59 -0700

    fix users.json reference to use path module for all

--- scripts/server.js
+++ scripts/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require('path');
 const cors = require("cors");
 
 const app = express();
@@ -7,17 +8,17 @@ app.use(express.json());
 app.use(cors());
 
 function getUsers() {
-    return JSON.parse(fs.readFileSync("../users.json"));
+    return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../users.json')));
 }
 
 app.post("/login", (req, res) => {
-    const { username, password } = req.body;
+    const {username, password} = req.body;
     const users = getUsers();
 
     const user = users.find(u => u.username === username && u.password === password);
 
     if (!user) {
-        return res.status(401).json({ success: false, message: "Invalid login" });
+        return res.status(401).json({success: false, message: "Invalid login"});
     }
 
     res.json({
